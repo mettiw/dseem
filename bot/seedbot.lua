9c7f4b7178b9d52db9251019805862fc91ff1666
moltafet35 <moltafet@mailfa.com>
2015-12-30 11:18:39 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -225,23 +225,8 @@ function create_config( )
     realm = {},--Realms Id
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v1
-An advance Administration bot based on yagop/telegram-bot 
-
-https://github.com/SEEDTEAM/TeleSeed
-
-Admins
-@iwals [Founder]
-@imandaneshi [Developer]
-@seyedan25 [Manager]
-
-Special thanks to
-awkward_potato
-Siyanew
-topkecleon
-Vamptacus
-
-Our channels
-@teleseedch [English]
+Tnx to SEEDTeam
+Created by DARKNESS TM
 ]],
     help_text = [[
 Commands list :
